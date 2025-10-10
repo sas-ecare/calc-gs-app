0ab52197f2d27014a1aba0eb7853a0e7d016dfa5
Gustavo <149829646+gustavo3-freitas@users.noreply.github.com>
2025-10-10 15:07:07 -0300

    Update app_calculadora_ganhos.py

--- app_calculadora_ganhos.py
+++ app_calculadora_ganhos.py
@@ -93,7 +93,7 @@ def get_volumes(df, segmento, subcanal, anomes):
     ].sum()
 
     vol_41 = df_f.loc[
-        df_f["NM_KPI"].str.contains("4", case=False, na=True),
+        df_f["NM_KPI"].str.contains("4.1", case=True, na=True),
         "VOL_KPI"
     ].sum()
 
@@ -293,6 +293,7 @@ if st.button("🚀 Calcular Ganhos Potenciais"):
     st.download_button("📥 Baixar Excel Completo", buffer.getvalue(),
                        file_name="simulacao_cr.xlsx",
                        mime="application/vnd.ms-excel")
+
 
 
 
