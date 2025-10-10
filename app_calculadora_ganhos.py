c6b26043fbfd3ddf5644cf023ba887072becb1a3
Gustavo <149829646+gustavo3-freitas@users.noreply.github.com>
2025-10-10 19:01:41 -0300

    Update app_calculadora_ganhos.py

--- app_calculadora_ganhos.py
+++ app_calculadora_ganhos.py
@@ -65,8 +65,9 @@ def normalize_text(s):
     return s.strip()
 
 # ====================== BASE ======================
-URL = "https://raw.githubusercontent.com/gustavo3-freitas/base_calculadora/main/Tabela_Performance_v2.xlsx"
 st.cache_data.clear()
+URL = "https://raw.githubusercontent.com/gustavo3-freitas/base_calculadora/main/Tabela_Performance_v2.xlsx"
+
 @st.cache_data(show_spinner=True)
 def carregar_dados():
     df = pd.read_excel(URL, sheet_name="Tabela Performance")
@@ -273,4 +274,5 @@ if st.button("🚀 Calcular Ganhos Potenciais"):
     st.download_button("📥 Baixar Excel Completo", buffer.getvalue(),
                        file_name="simulacao_cr.xlsx",
                        mime="application/vnd.ms-excel")
+
 
